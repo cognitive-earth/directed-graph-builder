18fbfbf5338d49ab333af3da7bc5d155b1c1b30f
jbangay <james.bangay@cognitive.earth>
2020-07-23 16:07:44 +1000

    Google CLoud Run

--- scripts/defaultApplicationCredentials.sh
+++ scripts/defaultApplicationCredentials.sh
@@ -2,3 +2,4 @@ gcloud config set project $1
 gcloud auth application-default login
 curl http://localhost:3004/api/builder/initiate
 gcloud compute project-info add-metadata --metadata google-compute-default-region='australia-southeast1'
+gcloud services enable run.googleapis.com
