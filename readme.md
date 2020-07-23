c4861ccd4f272a568aedddf304ad7cc6f2045cbd
jbangay <james.bangay@cognitive.earth>
2020-07-23 15:11:25 +1000

    Google CLoud Run

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ create a directed graph from links
 ```
 curl http://localhost:3004/api/builder/initiate
 ```
+
+[![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://deploy.cloud.run)
--- scripts/defaultApplicationCredentials.sh
+++ scripts/defaultApplicationCredentials.sh
@@ -1,5 +1,4 @@
 gcloud config set project $1
 gcloud auth application-default login
 curl http://localhost:3004/api/builder/initiate
-gcloud compute project-info add-metadata \
-    --metadata google-compute-default-region='australia-southeast1'
+gcloud compute project-info add-metadata --metadata google-compute-default-region='australia-southeast1'
